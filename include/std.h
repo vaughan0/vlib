e053d8add41afa6e4a575564c1bfcde55f6c274f
Vaughan Newton <newton.vaughan0@gmail.com>
2012-11-06 18:18:01 +0200

    Removed interface.h and fixed up std.h

--- include/std.h
+++ include/std.h
@@ -2,17 +2,17 @@
 #define STD_H_95ECC92A271C4F
 
 #define interface(name) \
-  typedef struct name##_Class* name##_Class; \
+  typedef struct name##_Class name##_Class; \
   typedef struct name { \
-    name##_Class _class; \
+    name##_Class* _class; \
   } name; \
-  struct name##_Class {
+  struct name##_Class
 
-#define call(obj, method, ...) ((obj)->_class->method((obj), #__VA_ARGS__))
+#define call(obj, method, ...) ((obj)->_class->method((obj), ##__VA_ARGS__))
 
 #define data(name) \
   typedef struct name name; \
-  struct name { 
+  struct name 
 
 #endif /* STD_H_95ECC92A271C4F */
 
