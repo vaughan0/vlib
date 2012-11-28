04a009481800698edfa8fc9da92d327e973e9744
Vaughan Newton <newton.vaughan0@gmail.com>
2012-11-28 16:51:14 +0200

    Test FINALLY

--- test/error.c
+++ test/error.c
@@ -4,13 +4,17 @@
 
 static int error_catch() {
   error_t caught;
+  bool cleanup = false;
   TRY {
     verr_raise(1234);
     caught = 0;
   } CATCH(err) {
     caught = err;
+  } FINALLY {
+    cleanup = true;
   } ETRY
   assertEqual(caught, 1234);
+  assertEqual(cleanup, true);
   return 0;
 }
 
