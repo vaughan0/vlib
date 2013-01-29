793b8e04b78fdbbfa2264b6387e84eb52a80bc95
Vaughan Newton <newton.vaughan0@gmail.com>
2013-01-29 14:07:23 +0200

    Added deque_fuzz test

--- test/deque.c
+++ test/deque.c
@@ -27,7 +27,32 @@ static int deque_basic() {
   return 0;
 }
 
+static int deque_fuzz() {
+  Deque d[1];
+  for (size_t cap = 1; cap <= 10; cap++) {
+    deque_init(d, sizeof(int), cap);
+
+    for (int i = 0; i < 5; i++) {
+      *(int*)deque_pushback(d) = 4 - i;
+    }
+    for (int i = 0; i < 5; i++) {
+      *(int*)deque_pushfront(d) = 5 + i;
+    }
+
+    for (int i = 0; i < 10; i++) {
+      int val = *(int*)deque_back(d);
+      deque_popback(d);
+      assertEqual(i, val);
+    }
+
+    assertEqual(0, deque_size(d));
+    deque_close(d);
+  }
+  return 0;
+}
+
 VLIB_SUITE(deque) = {
   VLIB_TEST(deque_basic),
+  VLIB_TEST(deque_fuzz),
   VLIB_END,
 };
