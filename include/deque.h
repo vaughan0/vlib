c4478f0544422672ebc02a68d0e9262ec3a39d65
Vaughan Newton <newton.vaughan0@gmail.com>
2013-01-29 14:26:35 +0200

    Added deque_empty function

--- include/deque.h
+++ include/deque.h
@@ -2,6 +2,7 @@
 #define DEQUE_H_95CF9713056D46
 
 #include <stddef.h>
+#include <stdbool.h>
 
 #include <vlib/std.h>
 
@@ -17,6 +18,10 @@ void deque_init(Deque* d, size_t elemsz, size_t capacity);
 void deque_close(Deque* d);
 
 size_t deque_size(Deque* d);
+
+static inline bool deque_empty(Deque* d) {
+  return d->_frontr == d->_backw;
+}
 
 void* deque_pushfront(Deque* d);
 void* deque_front(Deque* d);
