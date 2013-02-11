c1745fea39127b161edde263007d240cbba7bdbd
Vaughan Newton <newton.vaughan0@gmail.com>
2013-02-11 11:05:56 +0200

    Added flags

---
+++ include/flag.h
@@ -0,0 +1,47 @@
+#ifndef FLAG_H_D36A1C1C6AFA5C
+#define FLAG_H_D36A1C1C6AFA5C
+
+#include <stdbool.h>
+
+#include <vlib/std.h>
+#include <vlib/hashtable.h>
+#include <vlib/vector.h>
+
+struct Flags;
+
+data(FlagType) {
+  size_t  size;
+  bool    (*parse)(const char* src, void* ptr);    
+  void    (*print)(char buf[64], void* ptr);
+};
+
+typedef void (*UsageFunc)(struct Flags* flags, const char* selfname);
+
+data(Flag) {
+  const char* name;
+  const char* help;
+  FlagType*   type;
+  void*       default_value;
+  void*       ptr;
+};
+
+data(Flags) {
+  UsageFunc   usage;
+  Hashtable   flags[1];
+};
+
+void  flags_init(Flags* self);
+void  flags_close(Flags* self);
+
+// Adds a flag and returns a pointer to it's default value.
+void* add_flag(Flags* self, void* ptr, FlagType* type, const char* name, const char* help);
+
+void  flags_parse(Flags* self, int argc, char* const argv[], Vector* extra);
+
+/* Flag types */
+
+extern FlagType flagtype_int[1];
+extern FlagType flagtype_string[1];
+
+#endif /* FLAG_H_D36A1C1C6AFA5C */
+
---
+++ src/flag.c
@@ -0,0 +1,120 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <vlib/flag.h>
+#include <vlib/error.h>
+
+static void default_usage(Flags* flags, const char* selfname);
+
+void flags_init(Flags* self) {
+  hashtable_init(self->flags, hasher_fnv64str, equaler_str, sizeof(const char*), sizeof(Flag));
+  self->usage = default_usage;
+}
+void flags_close(Flags* self) {
+  int free_flag(void* _key, void* _data) {
+    Flag* flag = _data;
+    if (flag->default_value) free(flag->default_value);
+    return HT_CONTINUE;
+  }
+  hashtable_iter(self->flags, free_flag);
+  hashtable_close(self->flags);
+}
+
+void* add_flag(Flags* self, void* ptr, FlagType* type, const char* name, const char* help) {
+  Flag* flag = hashtable_insert(self->flags, &name);
+  flag->name = name;
+  flag->help = help;
+  flag->type = type;
+  flag->default_value = v_malloc(type->size);
+  flag->ptr = ptr;
+  return flag->default_value;
+}
+
+void flags_parse(Flags* self, int argc, char* const argv[], Vector* extra) {
+
+  // Initialize all flags to their default values
+  int init_flag(void* _key, void* _data) {
+    Flag* flag = _data;
+    memcpy(flag->ptr, flag->default_value, flag->type->size);
+    return HT_CONTINUE;
+  }
+  hashtable_iter(self->flags, init_flag);
+
+  for (int i = 0; i < argc; i++) {
+    char* arg = argv[i];
+    if (*arg == '-') {
+
+      if (*(++arg) == '-') arg++; // allow double-dash
+      char* name = arg;
+      char* value = strchr(arg, '=');
+      if (value) *(value++) = 0;
+      Flag* flag = hashtable_get(self->flags, &name);
+      if (flag) {
+        if (!flag->type->parse(value, flag->ptr)) {
+          fprintf(stderr, "Illegal value for %s: %s\n", name, value);
+          self->usage(self, argv[0]);
+          return;
+        }
+      } else {
+        fprintf(stderr, "Unknown argument: %s\n", name);
+        self->usage(self, argv[0]);
+        return;
+      }
+
+    } else if (extra) {
+      *(char**)vector_push(extra) = arg;
+    }
+  }
+}
+
+static void default_usage(Flags* flags, const char* selfname) {
+  fprintf(stderr, "Usage: %s\n", selfname);
+  if (flags->flags->size > 0) {
+    int print_flag(void* _key, void* _data) {
+      Flag* flag = _data;
+      char valbuf[64];
+      flag->type->print(valbuf, flag->default_value);
+      char cbuf[512];
+      snprintf(cbuf, sizeof(cbuf), "  -%s=%s", flag->name, valbuf);
+      fprintf(stderr, "%-20s  %s\n", cbuf, flag->help);
+      return HT_CONTINUE;
+    }
+    hashtable_iter(flags->flags, print_flag);
+  }
+  exit(1);
+}
+
+/* Flag types */
+
+static bool int_parse(const char* src, void* ptr) {
+  errno = 0;
+  char* endptr;
+  long int value = strtol(src, &endptr, 10);
+  if (errno != 0 || endptr == src) return false;
+  *(int*)ptr = value;
+  return true;
+}
+static void int_print(char buf[64], void* ptr) {
+  snprintf(buf, sizeof(buf), "%d", *(int*)ptr);
+}
+FlagType flagtype_int[1] = {{
+  .size = sizeof(int),
+  .parse = int_parse,
+  .print = int_print,
+}};
+
+static bool string_parse(const char* src, void* ptr) {
+  *(const char**)ptr = src;
+  return true;
+}
+static void string_print(char buf[64], void* ptr) {
+  snprintf(buf, sizeof(buf), "%s", *(const char**)ptr);
+}
+FlagType flagtype_string[1] = {{
+  .size = sizeof(const char*),
+  .parse = string_parse,
+  .print = string_print,
+}};
