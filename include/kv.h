7c16945e03fd89f830155873448c201667af81a0
Vaughan Newton <newton.vaughan0@gmail.com>
2013-02-26 20:25:56 +0200

    Fixed kv.h

---
+++ include/kv.h
@@ -0,0 +1,19 @@
+#ifndef KV_H_3EF446161DB48B
+#define KV_H_3EF446161DB48B
+
+#include <vlib/std.h>
+
+data(KVDatum) {
+  void*   ptr;
+  size_t  size;
+  size_t  max;
+};
+
+interface(KVDB) {
+  void    (*lookup)(void* self, KVDatum key, KVDatum* data);
+  void    (*store)(void* self, KVDatum key, KVDatum data);
+  void    (*close)(void* self);
+};
+
+#endif /* KV_H_3EF446161DB48B */
+
---
+++ src/kv.c
@@ -0,0 +1,3 @@
+
+#include <vlib/kv.h>
+
