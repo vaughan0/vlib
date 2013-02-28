b973ccc22eab800b47be47ed966bbfbabe8f14bd
Vaughan Newton <newton.vaughan0@gmail.com>
2013-02-28 14:31:44 +0200

    Added gdbm KVDB implementation

--- include/kv.h
+++ include/kv.h
@@ -9,10 +9,31 @@ data(KVDatum) {
 };
 
 interface(KVDB) {
+  /** Performs a lookup.
+   * If data->ptr is NULL, then data->size will be ignored and a new block
+   * of memory will be allocated and stored in data->ptr to hold the result of the operation.
+   *
+   * If data->ptr is not NULL, then the result will be stored in the given memory location. In
+   * this case, if the data is greater than data->size, a VERR_ARGUMENT exception will be raised.
+   *
+   * If the lookup fails, data->ptr will be set to NULL. If it succeeds, then data->size will be
+   * set to the size of the result data.
+   */
   void    (*lookup)(void* self, KVDatum key, KVDatum* data);
   void    (*store)(void* self, KVDatum key, KVDatum data);
   void    (*close)(void* self);
 };
+
+interface(KVOpener) {
+  KVDB*   (*open)(void* self, const char* table);
+  void    (*close)(void* self);
+};
+
+#ifdef VLIB_ENABLE_GDBM
+
+KVDB* kv_open_gdbm(const char* file);
+
+#endif
 
 #endif /* KV_H_3EF446161DB48B */
 
--- src/kv.c
+++ src/kv.c
@@ -1,3 +1,75 @@
 
-#include <vlib/kv.h>
+#include <string.h>
+#include <stdlib.h>
 
+#include <vlib/kv.h>
+#include <vlib/error.h>
+
+#ifdef VLIB_ENABLE_GDBM
+
+#include <gdbm.h>
+
+data(GDBM_DB) {
+  KVDB      base;
+  GDBM_FILE dbf;
+};
+static KVDB_Impl gdbm_kv_impl;
+
+KVDB* kv_open_gdbm(const char* file) {
+  GDBM_FILE dbf = gdbm_open((char*)file, 0, GDBM_WRCREAT, 0644, NULL);
+  if (!dbf) verr_raise_system();
+  GDBM_DB* self = malloc(sizeof(GDBM_DB));
+  self->base._impl = &gdbm_kv_impl;
+  self->dbf = dbf;
+  return &self->base;
+}
+
+static void kv_gdbm_lookup(void* _self, KVDatum key, KVDatum* data) {
+  GDBM_DB* self = _self;
+  datum dbkey = {
+    .dptr = key.ptr,
+    .dsize = key.size,
+  };
+  datum result = gdbm_fetch(self->dbf, dbkey);
+  if (!result.dptr) {
+    data->ptr = NULL;
+    return;
+  }
+
+  if (data->ptr) {
+    if (result.dsize > data->size) {
+      free(result.dptr);
+      RAISE(ARGUMENT);
+    }
+    memcpy(data->ptr, result.dptr, result.dsize);
+    data->size = result.dsize;
+    free(result.dptr);
+  } else {
+    data->ptr = result.dptr;
+    data->size = result.dsize;
+  }
+}
+static void kv_gdbm_store(void* _self, KVDatum key, KVDatum data) {
+  GDBM_DB* self = _self;
+  datum dbkey = {
+    .dptr = key.ptr,
+    .dsize = key.size,
+  };
+  datum dbval = {
+    .dptr = data.ptr,
+    .dsize = data.size,
+  };
+  gdbm_store(self->dbf, dbkey, dbval, GDBM_REPLACE);
+}
+static void kv_gdbm_close(void* _self) {
+  GDBM_DB* self = _self;
+  gdbm_close(self->dbf);
+  free(self);
+}
+static KVDB_Impl gdbm_kv_impl = {
+  .lookup = kv_gdbm_lookup,
+  .store = kv_gdbm_store,
+  .close = kv_gdbm_close,
+};
+
+#endif
